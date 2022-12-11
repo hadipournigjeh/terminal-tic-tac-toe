30a414140bdb31f2f9c926c087f6a59d17dd8a4b
Baz <bazmurphy@gmail.com>
2022-12-11 15:21:40 +0000

    completed all 3 files and functions

--- board-printer.js
+++ board-printer.js
@@ -16,7 +16,17 @@
     Test your function by calling it with an example tic-tac-toe board.
 */
 export function printBoard(board) {
+    for (let rows of board) {
+        console.log(`  ${rows[0] !== "_" ? rows[0] : " "}  |  ${rows[1] !== "_" ? rows[1] : " "}  |  ${rows[2] !== "_" ? rows[2] : " "}\n=================`)
+    }
 }
+// TESTS FOR printBoard
+// let board = [
+//     ['X', '_', '_'],
+//     ['_', 'X', '_'],
+//     ['O', 'O', 'X']
+// ];
+// printBoard(board);
 
 /*
     Given a tic-tac-toe board (an array of arrays),
@@ -24,4 +34,23 @@ export function printBoard(board) {
         - return false if there are still moves that can be made
 */
 export function checkIfNoMovesLeft(board) {
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            if (board[i][j] === "_") {
+                return false;
+            }
+        }
+    }
+    return true;
 }
+// TESTS FOR checkIfNoMovesLeft
+// let board1 =    [['X', '_', '_'],
+//                 ['_', 'X', '_'],
+//                 ['O', 'O', 'X']];
+// let board2 =    [['X', 'X', 'X'],
+//                 ['X', 'X', 'X'],
+//                 ['O', 'O', 'X']];
+// console.log(checkIfNoMovesLeft(board1));
+// // false
+// console.log(checkIfNoMovesLeft(board2));
+// // true
--- status-checker.js
+++ status-checker.js
@@ -18,7 +18,18 @@ import { checkIfNoMovesLeft } from './board-printer.js';
     Otherwise, return false
 */
 function checkRow(board, player, rowNumber) {
+    return board[rowNumber].every(element => element === player);
 }
+// TESTS FOR checkRow
+// let boardTest1 = [
+//     ['_', '_', '_'],
+//     ['_', '_', '_'],
+//     ['O', 'O', 'O']
+// ];
+// console.log(checkRow(boardTest1, "O", 2));
+// true
+// console.log(checkRow(boardTest1, "O", 1));
+// false
 
 /*
     Given 3 parameters:
@@ -29,7 +40,26 @@ function checkRow(board, player, rowNumber) {
     Otherwise, return false
 */
 function checkColumn(board, player, columnNumber) {
+    // 0,0 1,0 2,0 -- far left column indices
+    // 0,1 1,1 2,1 -- middle column indices
+    // 0,2 1,2 2,2 -- right column indices
+    for (let row in board) {
+        if (board[row][columnNumber] !== player) {
+            return false;
+        }
+    }
+    return true;
 }
+// TESTS FOR checkColumn
+// let boardTest2 = [
+//     ['_', '_', 'O'],
+//     ['_', '_', 'O'],
+//     ['_', '_', 'O']
+// ];
+// console.log(checkColumn(boardTest2, "O", 2));
+// true
+// console.log(checkColumn(boardTest2, "O", 1));
+// false
 
 /*
     Given 2 parameters:
@@ -40,8 +70,45 @@ function checkColumn(board, player, columnNumber) {
 */
 function checkDiagonal(board, player) {
     // It may be easier to use an if statement than a loop here
-}
 
+    // 0,0 1,1 2,2 -- top left to bottom right diagonal indices
+    // 0,2 1,1 2,0 -- top right to bottom left diagonal indices
+    if (
+        board[0][0] === player &&
+        board[1][1] === player &&
+        board[2][2] === player ||
+        board[0][2] === player &&
+        board[1][1] === player &&
+        board[2][0] === player
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+    // ^ lets try to solve this with a loop... challenging...
+}
+// TESTS FOR checkColumn
+// let boardTest3 = [
+//     ['O', '_', '_'],
+//     ['_', 'O', '_'],
+//     ['_', '_', 'O']
+// ];
+// let boardTest4 = [
+//     ['_', '_', 'O'],
+//     ['_', 'O', '_'],
+//     ['O', '_', '_']
+// ];
+// let boardTest5 = [
+//     ['O', '_', 'O'],
+//     ['_', '_', '_'],
+//     ['O', '_', 'O']
+// ];
+// console.log(checkDiagonal(boardTest3, "O"));
+// true
+// console.log(checkDiagonal(boardTest4, "O"));
+// true
+// console.log(checkDiagonal(boardTest5, "O"));
+// false
 
 /*
     There is no need to change any code below this line.
